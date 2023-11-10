2caefaf58efe5c73251295a650f13e53da1a7a59
Alex Uldahl Pedersen <alex@uldahl.dk>
2023-11-10 10:05:53 +0100

    Update WebsocketController.cs

--- backend/ws/Controllers/WebsocketController.cs
+++ backend/ws/Controllers/WebsocketController.cs
@@ -2,40 +2,50 @@ using System.Net.WebSockets;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ws.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class WebSocketController() : ControllerBase
+namespace ws.Controllers
 {
-    private static readonly List<WebSocket> Websockets = new();
-
-    [Route("/ws")]
-    public async Task Get()
+    [ApiController]
+    [Route("[controller]")]
+    public class WebSocketController : ControllerBase
     {
-        var context = HttpContext;
-        using var socket = await context.WebSockets.AcceptWebSocketAsync();
+        private static readonly List<WebSocket> websockets = new List<WebSocket>();
 
-        Console.WriteLine(" -> A new client connected!");
-
-        bool connectionAlive = true;
-        List<byte> webSocketPayload = new List<byte>(1024 * 4); // 4 KB initial capacity
-        webSocketPayload.Clear();
-        byte[] tempMessage = new byte[1024 * 4]; // Message reader
-
-    
-        WebSocketReceiveResult? webSocketResponse = await socket.ReceiveAsync(tempMessage, CancellationToken.None);
-
-        webSocketPayload.AddRange(new ArraySegment<byte>(tempMessage, 0, webSocketResponse.Count));
-
-        
-        string message = Encoding.UTF8.GetString(webSocketPayload.ToArray());
-        Websockets.Add(socket);
-        Console.WriteLine("Client says {0}", message);
-        var bf = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message + " RESPONSE"));
-        foreach (var client in Websockets)
+        [Route("/ws")]
+        public async Task Get()
         {
-            await client.SendAsync(bf, WebSocketMessageType.Text, true, CancellationToken.None);
+            var context = HttpContext;
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                Console.WriteLine(" -> A new client connected!");
+
+                websockets.Add(webSocket);
+                
+                await EchoLoop(webSocket);
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+            }
+        }
+
+        private async Task EchoLoop(WebSocket webSocket)
+        {
+            byte[] buffer = new byte[1024 * 4]; //4KB buffer size
+            while (webSocket.State == WebSocketState.Open)
+            {
+                WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                foreach (var client in websockets)
+                {
+                    if(client.State == WebSocketState.Open)
+                    {
+                        await client.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+                    }
+                }
+            }
+            websockets.Remove(webSocket);
+            await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
         }
     }
 }
