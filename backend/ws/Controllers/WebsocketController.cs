ed39101b58acffe656412772d939708d7270cbf1
Alex Uldahl Pedersen <alex@uldahl.dk>
2023-11-10 10:20:41 +0100

    Update WebsocketController.cs

--- backend/ws/Controllers/WebsocketController.cs
+++ backend/ws/Controllers/WebsocketController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Net.WebSockets;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
@@ -8,20 +10,25 @@ namespace ws.Controllers
     [Route("[controller]")]
     public class WebSocketController : ControllerBase
     {
-        private static readonly List<WebSocket> websockets = new List<WebSocket>();
+        private static readonly ConcurrentDictionary<string, List<WebSocket>> rooms = new ConcurrentDictionary<string, List<WebSocket>>();
 
-        [Route("/ws")]
-        public async Task Get()
+        [Route("/ws/{room}")]
+        public async Task Get(string room)
         {
             var context = HttpContext;
             if (context.WebSockets.IsWebSocketRequest)
             {
                 WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
-                Console.WriteLine(" -> A new client connected!");
-
-                websockets.Add(webSocket);
                 
-                await EchoLoop(webSocket);
+                if (!rooms.ContainsKey(room))
+                {
+                    rooms[room] = new List<WebSocket>();
+                }
+                rooms[room].Add(webSocket);
+
+                Console.WriteLine($" -> A new client connected to room: {room}");
+
+                await EchoLoop(webSocket, room);
             }
             else
             {
@@ -29,23 +36,33 @@ namespace ws.Controllers
             }
         }
 
-        private async Task EchoLoop(WebSocket webSocket)
+        private async Task EchoLoop(WebSocket webSocket, string room)
         {
-            byte[] buffer = new byte[1024 * 4]; //4KB buffer size
+            var buffer = new byte[1024 * 4];
             while (webSocket.State == WebSocketState.Open)
             {
-                WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
 
-                foreach (var client in websockets)
+                foreach (var client in rooms[room])
                 {
-                    if(client.State == WebSocketState.Open)
+                    if (client.State == WebSocketState.Open && result.MessageType != WebSocketMessageType.Close)
                     {
                         await client.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
                     }
                 }
             }
-            websockets.Remove(webSocket);
-            await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+            Console.WriteLine($" -> A client disconnected from room: {room}");
+            // Construct a disconnect message
+            string disconnectMessage = "A client has disconnected!";
+            var disconnectMessageBuffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(disconnectMessage));
+            rooms[room].Remove(webSocket);
+            foreach (var client in rooms[room])
+            {
+                if (client.State == WebSocketState.Open)
+                {
+                    await client.SendAsync(disconnectMessageBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+            }  
         }
     }
 }
