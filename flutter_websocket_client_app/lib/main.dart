c3629f84f995a6c0552bbe315eca5596bbe14e3e
Alex Uldahl Pedersen <alex@uldahl.dk>
2023-12-09 10:42:12 +0100

    websocket client app from flutter

---
+++ flutter_websocket_client_app/lib/main.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: avoid_print
+import 'package:web_socket_client/web_socket_client.dart';
+
+void main() async {
+  // Create a WebSocket client.
+  final uri = Uri.parse('ws://localhost:8181');
+  const backoff = ConstantBackoff(Duration(seconds: 1));
+  final socket = WebSocket(uri, backoff: backoff);
+
+  // Listen for changes in the connection state.
+  socket.connection.listen((state) => print('state: "$state"'));
+
+  // Listen for incoming messages.
+  socket.messages.listen((message) {
+    print('message: "$message"');
+
+    // Send a message to the server.
+    socket.send('ping');
+
+  });
+
+  await Future<void>.delayed(const Duration(seconds: 3));
+
+  // Close the connection.
+  socket.send("hello from flutter!");
+}
