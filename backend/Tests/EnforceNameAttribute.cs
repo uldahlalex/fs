5a80252bfc01ab3abd469b06e3faca22277bbe99
Alex Uldahl Pedersen <alex@uldahl.dk>
2024-01-16 23:05:54 +0100

    Update EnforceNameAttribute.cs

--- backend/Tests/EnforceNameAttribute.cs
+++ backend/Tests/EnforceNameAttribute.cs
@@ -1,25 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-using System.Reflection;
+
 
 namespace Tests;
 
-[AttributeUsage(AttributeTargets.All)] //todo implement test
-public class EnforceNameAttribute(string name) : ValidationAttribute
-{
-    public string Name { get; } = name;
-}
-
 public static class EnforceNameCheck
 {
-    public static void CheckPropertyNames<T>()
-    {
-        var typeInfo = typeof(T).GetTypeInfo();
-        foreach (var propertyInfo in typeInfo.DeclaredProperties)
-        {
-            var enforceNameAttribute = propertyInfo.GetCustomAttribute<EnforceNameAttribute>();
-            if (enforceNameAttribute != null && enforceNameAttribute.Name != propertyInfo.Name)
-                throw new ValidationException(
-                    $"Property named '{propertyInfo.Name}' violated the naming rule. It should be named '{enforceNameAttribute.Name}'.");
-        }
-    }
+  
 }
