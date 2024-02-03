b94ad0cb04685bb4fedde75558e42523d965eaa6
Alex Uldahl Pedersen <alex@uldahl.dk>
2024-02-03 21:19:24 +0100

    Update ValidateDataAnnotations.cs

--- backend/api/Attributes/EventFilters/ValidateDataAnnotations.cs
+++ backend/api/Attributes/EventFilters/ValidateDataAnnotations.cs
@@ -9,7 +9,7 @@ public class ValidateDataAnnotations : BaseEventFilter
     public override Task Handle<T>(IWebSocketConnection socket, T dto)
     {
         
-            var validationContext = new ValidationContext(dto ?? throw new ArgumentNullException(nameof(model)));
+            var validationContext = new ValidationContext(dto ?? throw new ArgumentNullException(nameof(dto)));
             Validator.ValidateObject(dto, validationContext, true);
             return Task.CompletedTask;
     }
